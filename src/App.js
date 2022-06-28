eb91aac526ceeb29e3e05e6e8d120af61ea9ad59
hedbewd <hedbewd@gmail.com>
2022-06-28 12:00:34 +0900

    file upload button and fix App.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,13 @@ import React from 'react';
 import SiteName from './SiteName';
 import TransactionAdd from './TransactionAdd';
 import TransactionShow from './TransactionShow';
+import TransactionInput from './TransactionInput';
 
 function App() {
   return (
     <div>
       <SiteName />
-      <input type="text" placeholder="Type"></input>
-      <br></br>
-      <input type="text" placeholder="Name"></input>
-      <br></br>
-      <input type="text" placeholder="Time"></input>
-      <br></br>
-      <input type="text" placeholder="IPFS Hash"></input>
-      <br></br>
-      <input type="text" placeholder="Registrant"></input>
-      <br></br>
-      <input type="text" placeholder="Responsible Manager"></input>
-      <br></br>
-      <input type="text" placeholder="File Type"></input>
-      <br></br>
-      <input type="text" placeholder="File Description"></input>
-      <br></br>
+      <TransactionInput></TransactionInput>
       <TransactionAdd>트랜잭션 추가</TransactionAdd>
       <TransactionShow>트랜잭션 보기</TransactionShow>
     </div>
